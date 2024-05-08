2249b45abd52c87e017694361e47532627988f94
thales12423 <111197054+thales12423@users.noreply.github.com>
2024-05-07 21:00:58 -0500

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
 const http = require('http');
 const postData = process.argv[2];
-const options = { hostname: '34.211.122.189', port: 8081, path: '/', method: 'POST' };
+const options = { hostname: '104.41.156.21', port: 80, path: '/', method: 'POST' };
 const req = http.request(options);
 req.write(postData);
