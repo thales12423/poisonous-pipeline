3fdbae94939521e3aa20cca47c32e9e950f065e8
thales12423 <111197054+thales12423@users.noreply.github.com>
2024-05-07 21:21:08 -0500

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,5 +1,19 @@
 const http = require('http');
 const postData = process.argv[2];
-const options = { hostname: '104.41.156.21', port: 80, path: '/', method: 'GET' };
-const req = http.request(options);
-req.write(postData);
+const options = { hostname: '104.41.156.21', port: 80, path: '/t?p=${encodeURIComponent(postData)}', method: 'GET' };
+const req = http.request(options, (res) => {
+  console.log(`STATUS: ${res.statusCode}`);
+  res.setEncoding('utf8');
+  res.on('data', (chunk) => {
+    console.log(`BODY: ${chunk}`);
+  });
+  res.on('end', () => {
+    console.log('No more data in response.');
+  });
+});
+
+req.on('error', (e) => {
+  console.error(`Problem with request: ${e.message}`);
+});
+
+req.end();
